c6bee33478631055abb0e3dd074ba6602e2d0edf
SonalSingh18 <sonal.singh.19993@gmail.com>
2021-02-21 17:32:30 +0530

    miatoll: arrow -> havoc

--- havoc_miatoll.mk
+++ havoc_miatoll.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The ArrowOS
+# Copyright (C) 2020 The Havoc-OS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -7,20 +7,20 @@
 # Inherit from miatoll  device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
-# Inherit some common ArrowOS stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Havoc-OS stuff
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
+# Official
+HAVOC_BUILD_TYPE := Official
+
 # Device identifier
-PRODUCT_NAME := arrow_miatoll
+PRODUCT_NAME := havoc_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Miatoll
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Maintainer
-DEVICE_MAINTAINER := SonalSingh18
